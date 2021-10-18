64cd6886366d594fdb93f78f8017fb82ce46578c
trachman <tsrachman7@gmail.com>
2021-10-18 19:22:27 -0400

    draws triangle onto the screen with legacy opengl

--- src/quasi.cpp
+++ src/quasi.cpp
@@ -27,6 +27,13 @@ int main()
 		// render here
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// legacy opengl
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
 		// swap front and back buffers
 		glfwSwapBuffers(window);
 
