76fdb0b1d370ea4d23e16478f613148574123662
trachman <tsrachman7@gmail.com>
2021-10-18 23:32:24 -0400

    finished episode 6 of opengl cherno series

--- src/quasi.cpp
+++ src/quasi.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 // glew is used to extract drivers for opengl
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -27,6 +28,24 @@ int main()
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error occurred with glewInit()" << std::endl;
 
+	// print opengl version
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	// ModernOpenGl Implemenation
+	float positions[] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);	
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+
 	// loop until the user closes the window
 	while(!glfwWindowShouldClose(window))
 	{
@@ -34,11 +53,13 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// legacy opengl
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		// glBegin(GL_TRIANGLES);
+		// glVertex2f(-0.5f, -0.5f);
+		// glVertex2f(0.0f, 0.5f);
+		// glVertex2f(0.5f, -0.5f);
+		// glEnd();
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// swap front and back buffers
 		glfwSwapBuffers(window);
